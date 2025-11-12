4de3df4be8a0e57a0b240c51b808b8f355d6536c
TRMrDucky <vv094673@gmail.com>
2025-11-11 21:23:28 -0700

    un poquito màs de estructura

---
+++ happySource_webApp/src/main/java/com/mycompany/happysource_webapp/persistencia/Review.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.happysource_webapp.persistencia;
+
+/**
+ *
+ * @author vv094
+ */
+public class Review {
+    
+}
---
+++ happySource_webApp/src/main/java/com/mycompany/happysource_webapp/persistencia/TipoUsuario.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.happysource_webapp.persistencia;
+
+/**
+ *
+ * @author vv094
+ */
+public enum TipoUsuario {
+    CLIENTE,
+    ADMINISTRADOR
+}
