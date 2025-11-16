2e3d3b686fefa827f99ad62f850dacccdabdf17f
TRMrDucky <vv094673@gmail.com>
2025-11-15 20:46:29 -0700

    cambios menores

--- HappySourcePersistencia/src/main/java/entidades/UsuarioAdmin.java
+++ HappySourcePersistencia/src/main/java/entidades/UsuarioAdmin.java
@@ -4,10 +4,76 @@
  */
 package entidades;
 
+import Enumeradores.Rol;
+
 /**
  *
  * @author vv094
  */
 public class UsuarioAdmin {
-    
+    private String nombre;
+    private String correo;
+    private String contraseña;
+    private String telefono;
+    private String direccion;
+    private Rol rol;
+
+    public UsuarioAdmin(String nombre, String correo, String contraseña, String telefono, String direccion, Rol rol) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+
 }
